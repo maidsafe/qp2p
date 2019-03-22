2622c293c224dc0e2d484ab1b2df223deb8964e0
Povilas Balciunas <balciunas90@gmail.com>
2019-03-22 11:45:45 +0200

    Initial client node implementation

--- examples/common/mod.rs
+++ examples/common/mod.rs
@@ -2,7 +2,7 @@ use serde_derive::{Deserialize, Serialize};
 use using_quinn::CrustInfo;
 
 /// Remote procedure call for our examples to communicate.
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Rpc {
     /// Starts the connectivity and data exchange test between us and given Crust peers.
     StartTest(Vec<CrustInfo>),
