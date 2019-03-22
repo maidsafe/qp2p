809e8e048a310e33e2d66f7889fd596b58bb1e75
Nikita Baksalyar <nikita.baksalyar@maidsafe.net>
2019-03-22 11:27:56 +0000

    Load boostrap node config from file
    Also don't require an echo service to get our connection info

--- examples/bootstrap_node.rs
+++ examples/bootstrap_node.rs
@@ -7,6 +7,7 @@
 //! $ RUST_LOG=bootstrap_node=info cargo run --example bootstrap_node
 //! ```
 
+extern crate config_file_handler;
 #[macro_use]
 extern crate log;
 #[macro_use]
@@ -17,31 +18,71 @@ extern crate serde_derive;
 mod common;
 use common::Rpc;
 
-use using_quinn::{Config, Crust, Event};
+use using_quinn::{Config, Crust, CrustInfo, Event, SerialisableCertificate};
 
 use bincode;
+use config_file_handler::FileHandler;
 use env_logger;
 use serde_json;
 use std::collections::HashMap;
 use std::sync::mpsc::channel;
+use std::{io, net::Ipv4Addr};
 
-fn main() {
+#[derive(Serialize, Deserialize)]
+struct BootstrapNodeConfig {
+    ip: Ipv4Addr,
+    port: u16,
+}
+
+impl Default for BootstrapNodeConfig {
+    fn default() -> Self {
+        BootstrapNodeConfig {
+            ip: unwrap!("127.0.0.1".parse()),
+            port: 5000,
+        }
+    }
+}
+
+fn main() -> Result<(), io::Error> {
     env_logger::init();
 
+    // Initialise configuration
+    let cfg_file_handler = FileHandler::<BootstrapNodeConfig>::new("bootstrap.config", true)
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    let bootstrap_node_config = cfg_file_handler
+        .read_file()
+        .unwrap_or_else(|_e| BootstrapNodeConfig::default());
+
+    // Initialise Crust
     let (ev_tx, ev_rx) = channel();
 
-    let mut crust = Crust::with_config(
-        ev_tx,
-        Config {
-            port: Some(5000), // TODO(povilas): make port configurable
-            ..Default::default()
-        },
-    );
+    let (mut crust, our_cert_der) = {
+        let our_complete_cert = SerialisableCertificate::default();
+        let cert_der = our_complete_cert.cert_der.clone();
+        (
+            Crust::with_config(
+                ev_tx,
+                Config {
+                    our_complete_cert: Some(our_complete_cert),
+                    port: Some(bootstrap_node_config.port),
+                    ..Default::default()
+                },
+            ),
+            cert_der,
+        )
+    };
     crust.start_listening();
-    info!("Crust started");
+
+    info!("Crust started on port {}", bootstrap_node_config.port);
 
     // TODO(povilas): make our_connection_info use IGD crate when no stun servers configured
-    let our_conn_info = unwrap!(crust.our_connection_info());
+    let our_conn_info = CrustInfo {
+        peer_addr: unwrap!(format!("127.0.0.1:{}", bootstrap_node_config.port).parse()),
+        peer_cert_der: our_cert_der,
+    };
+
+    // let our_conn_info = unwrap!(crust.our_connection_info());
     println!(
         "Our connection info:\n{}\n",
         unwrap!(serde_json::to_string(&our_conn_info)),
@@ -74,4 +115,6 @@ fn main() {
             event => warn!("Unexpected event: {:?}", event),
         }
     }
+
+    Ok(())
 }
