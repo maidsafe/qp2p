d7efda1c785482d6b3cc5ecc62076c26e608e6b8
Nikita Baksalyar <nikita.baksalyar@maidsafe.net>
2019-04-09 15:10:16 +0100

    Update client example to send small msgs along with large

--- auto-test.sh
+++ auto-test.sh
@@ -5,11 +5,12 @@
 
 export RUST_LOG=client_node=info
 
+tmux kill-session -t crust
 tmux new-session -s crust -d
 
 for i in {1..50}
 do
   echo "Running client $i ..."
   tmux new-window -t crust:$i -n "client$i"
-  tmux send-keys -t crust:$i "RUST_BACKTRACE=1 cargo run --release --example client_node -b '$1'" C-m
+  tmux send-keys -t crust:$i "RUST_LOG=client_node=trace RUST_BACKTRACE=1 cargo run --release --example client_node -- -b '$1'" C-m
 done
