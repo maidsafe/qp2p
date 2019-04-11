e73a6b3fc015f885c9189a8fdd8f571956b68c69
Stephen Coyle <stephen.coyle@maidsafe.net>
2019-04-11 15:21:03 +0100

    chore/license: add license details to repo files

--- examples-bkup/bootstrap_node.rs
+++ examples-bkup/bootstrap_node.rs
@@ -1,3 +1,12 @@
+// Copyright 2019 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
+
 //! Bootstrap node which acts as a relay for other client nodes. It collects the info of multiple
 //! client nodes and relays it to all remaining connected nodes, hence allows them all to connect
 //! with each other.
