1b62471ae09544514d1085e0d417b692c15fc3f6
Stephen Coyle <stephen.coyle@maidsafe.net>
2019-04-11 15:21:03 +0100

    chore/license: add license details to repo files

--- examples-bkup/chat.rs
+++ examples-bkup/chat.rs
@@ -1,3 +1,12 @@
+// Copyright 2019 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
+
 //! Basic chat like example that demonstrates how to connect with peers and exchange data.
 
 #[macro_use]
