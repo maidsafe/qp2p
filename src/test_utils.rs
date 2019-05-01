838d651bcf766b3c1173f2370677f434cdca7649
Nikita Baksalyar <nikita.baksalyar@maidsafe.net>
2019-05-01 14:05:46 +0100

    Improve test_utils
    - Add function to delay outgoing connections. For example, this allows us to test reverse
    connections functionality.
    
    - Add a function to get a list of connections of a peer from its context.

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -8,28 +8,147 @@
 // Software.
 
 use crate::config::{Config, SerialisableCertificate};
-use crate::connection::{FromPeer, QConn, ToPeer};
+use crate::connection::{Connection, FromPeer, QConn, ToPeer};
 use crate::context::ctx;
+use crate::context::Context;
 use crate::dirs::{Dirs, OverRide};
 use crate::event::Event;
+use crate::utils::R;
 use crate::wire_msg::WireMsg;
 use crate::{communicate, Builder, NodeInfo, Peer, QuicP2p};
 use rand::Rng;
-use std::collections::HashSet;
-use std::env;
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::path::PathBuf;
 use std::sync::mpsc::{self, Receiver};
-use tokio::prelude::Future;
+use std::{
+    env,
+    ops::Deref,
+    time::{Duration, Instant},
+};
+use tokio::prelude::{future::Either, Future};
 use tokio::runtime::current_thread;
+use tokio::timer::Delay;
+
+thread_local! {
+    static TEST_CTX: RefCell<Option<TestContext>> = RefCell::new(None);
+}
+
+/// Obtain a mutable referece to the `TestContext`. This initialise `TestContext` with a default value
+/// if it has not been set in the current thread context.
+fn test_ctx_mut<F, R>(f: F) -> R
+where
+    F: FnOnce(&mut TestContext) -> R,
+{
+    TEST_CTX.with(|ctx_refcell| {
+        let mut ctx = ctx_refcell.borrow_mut();
+        if let Some(ctx) = ctx.as_mut() {
+            f(ctx)
+        } else {
+            let mut new_ctx = Default::default();
+            let res = f(&mut new_ctx);
+            *ctx = Some(new_ctx);
+            res
+        }
+    })
+}
+
+/// Obtain a reference to the `TestContext`. This will use the default `TestContext` if it has not been set
+/// in the current thread context.
+fn test_ctx<F, R>(f: F) -> R
+where
+    F: FnOnce(&TestContext) -> R,
+{
+    TEST_CTX.with(|ctx_refcell| {
+        let ctx = ctx_refcell.borrow();
+        if let Some(ctx) = ctx.as_ref() {
+            f(ctx)
+        } else {
+            f(&Default::default())
+        }
+    })
+}
+
+/// Newtype wrapper around `quinn::Endpoint` to allow creating artificial delays for outgoing connections.
+pub(crate) struct EndpointWrap<'a>(&'a quinn::Endpoint);
+
+impl<'a> EndpointWrap<'a> {
+    /// Wraps around `quinn::Endpoint::connect_with` and optionally creates an artificial delay.
+    /// Use `test_ctx` to set the delay.
+    pub fn connect_with(
+        &self,
+        config: quinn::ClientConfig,
+        addr: &SocketAddr,
+        server_name: &str,
+    ) -> Result<
+        impl Future<
+            Item = (
+                quinn::ConnectionDriver,
+                quinn::Connection,
+                quinn::IncomingStreams,
+            ),
+            Error = quinn::ConnectionError,
+        >,
+        quinn::ConnectError,
+    > {
+        let connecting_res = self.0.connect_with(config, addr, server_name)?;
+        let delay_ms = test_ctx(|ctx| ctx.connect_delay);
+        if delay_ms > 0 {
+            Ok(Either::A(
+                Delay::new(Instant::now() + Duration::from_millis(delay_ms))
+                    .then(move |_| connecting_res),
+            ))
+        } else {
+            Ok(Either::B(connecting_res))
+        }
+    }
+}
+
+impl<'a> Deref for EndpointWrap<'a> {
+    type Target = quinn::Endpoint;
+
+    fn deref(&self) -> &quinn::Endpoint {
+        self.0
+    }
+}
+
+impl Context {
+    pub(crate) fn quic_ep(&self) -> EndpointWrap {
+        EndpointWrap(&self.quic_ep)
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct TestContext {
+    connect_delay: u64,
+}
 
 /// Extend `QuicP2p` with test functions.
 impl QuicP2p {
+    pub(crate) fn set_connect_delay(&mut self, delay_ms: u64) {
+        self.el
+            .post(move || test_ctx_mut(|ctx| ctx.connect_delay = delay_ms));
+    }
+
     /// Send an arbitrary message. Used for testing malicious nodes and clients.
     pub(crate) fn send_wire_msg(&mut self, peer: Peer, msg: WireMsg) {
         self.el.post(move || {
             communicate::try_write_to_peer(peer, msg);
         });
+    }
+
+    /// Returns a list of connections of this peer.
+    pub(crate) fn connections<F: Send + 'static, Res: Send + 'static>(&mut self, f: F) -> R<Res>
+    where
+        F: FnOnce(&HashMap<SocketAddr, Connection>) -> Res,
+    {
+        let (tx, rx) = mpsc::channel();
+        self.el.post(move || {
+            let res = ctx(|c| f(&c.connections));
+            let _ = tx.send(res);
+        });
+        Ok(rx.recv()?)
     }
 }
 
