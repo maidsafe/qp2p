5ea6ca2a5090e0ff38e4d93c83ccd45491c097eb
Lionel Faber <lionel1704@gmail.com>
2020-09-25 19:37:15 +0530

    test(echo_service): complete echo service and upnp impl. with tests

--- src/utils.rs
+++ src/utils.rs
@@ -14,8 +14,10 @@ use crate::{
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::fs::File;
-use std::io::{BufReader, BufWriter};
+use std::io::{BufReader, BufWriter, Write};
 use std::path::Path;
+use flexi_logger::{DeferredNow, Logger};
+use log::Record;
 
 /// Get the project directory
 #[cfg(any(
@@ -91,4 +93,27 @@ where
         .and_then(|mut rdr| bincode::serialize_into(&mut rdr, s))?;
 
     Ok(())
+}
+
+pub(crate) fn init_logging() {
+        // Custom formatter for logs
+        let do_format = move |writer: &mut dyn Write, clock: &mut DeferredNow, record: &Record| {
+            let handle = std::thread::current();
+            write!(
+                writer,
+                "[{}] {} {} [{}:{}] {}",
+                handle
+                    .name()
+                    .unwrap_or(&format!("Thread-{:?}", handle.id())),
+                record.level(),
+                clock.now().to_rfc3339(),
+                record.file().unwrap_or_default(),
+                record.line().unwrap_or_default(),
+                record.args()
+            )
+        };
+
+        Logger::with_env()
+            .format(do_format)
+            .suppress_timestamp().start().map(|_| ()).unwrap_or(());
 }
