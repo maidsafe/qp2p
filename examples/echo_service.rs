fcf1ea2a1d4bd1af971746637af3f908faaf333e
Lionel Faber <lionel1704@gmail.com>
2021-01-06 16:00:39 +0530

    refactor(example): refactor echo service example to use StructOpt and remove chat functionality

--- examples/echo_service.rs
+++ examples/echo_service.rs
@@ -1,106 +1,94 @@
-use anyhow::{anyhow, bail, Error, Result};
-use bytes::Bytes;
-use qp2p::{Config, Message, QuicP2p};
-use std::env;
-use tokio::io::AsyncBufReadExt;
+use anyhow::{anyhow, Error, Result};
+use qp2p::{Config, QuicP2p};
+use std::collections::HashSet;
+use std::net::Ipv4Addr;
+use std::net::{IpAddr, SocketAddr};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt, Eq, PartialEq)]
+#[structopt(rename_all = "kebab-case")]
+pub struct Args {
+    /// Hard Coded contacts
+    /// A node will attempt to use the connection information in this list
+    /// to bootstrap to the network.
+    #[structopt(
+        short,
+        long,
+        default_value = "[]",
+        parse(try_from_str = serde_json::from_str)
+    )]
+    pub hard_coded_contacts: HashSet<SocketAddr>,
+    /// Port we want to reserve for QUIC. If none supplied we'll use the OS given random port.
+    /// This will be the internal port number mapped to the process
+    #[structopt(short, long)]
+    pub port: Option<u16>,
+    /// IP address for the listener. If none supplied and `forward_port` is enabled, we will use IGD to realize the
+    /// local IP address of the machine. If IGD fails the application will exit.
+    #[structopt(long)]
+    pub ip: Option<IpAddr>,
+    /// Specify if port forwarding via UPnP should be done or not. This can be set to false if the network
+    /// is run locally on the network loopback or on a local area network.
+    #[structopt(long)]
+    pub forward_port: bool,
+    /// Is the network meant to run on the local loopback network?
+    /// If this is set to true, the IP address will be set to `127.0.0.1`
+    #[structopt(long)]
+    pub local: bool,
+    /// Set to true if this is the first or genesis node
+    #[structopt(long)]
+    pub first: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
+    let mut args = Args::from_args();
 
-    let (bootstrap_nodes, genesis) = match &args[1][..] {
-        "create" => (vec![], true),
-        "connect" => {
-            let bootstrap_node = args[2]
-                .parse()
-                .map_err(|err| anyhow!("SocketAddr format not recognized: {}", err))?;
-            (vec![bootstrap_node], false)
-        }
-        other => {
-            bail!("Unexpected argument: {}", other);
-        }
-    };
+    if args.local {
+        args.ip = Some(IpAddr::V4(Ipv4Addr::LOCALHOST));
+        args.forward_port = false;
+    }
 
     let qp2p = QuicP2p::with_config(
         Some(Config {
-            ip: None,
-            port: Some(0),
-            forward_port: true,
+            ip: args.ip,
+            port: args.port,
+            forward_port: args.forward_port,
+            hard_coded_contacts: args.hard_coded_contacts,
             ..Default::default()
         }),
-        &bootstrap_nodes,
+        &[],
         false,
     )?;
 
-    let endpoint = qp2p.new_endpoint()?;
-    let socket_addr = endpoint.socket_addr().await?;
-    println!("Process running at: {}", &socket_addr);
+    let mut endpoint = qp2p.new_endpoint()?;
+    let local_addr = endpoint.local_addr();
+    
+    let public_addr = if args.forward_port {
+        endpoint.public_addr().await?
+    } else {
+        local_addr
+    };
 
-    if genesis {
-        println!("Waiting for connections");
+    println!(
+        "Node started.\nLocal adddress: {}\nPublic Address: {}",
+        local_addr,
+        public_addr        
+    );
+
+    loop {
+        println!("Waiting for connections...\n");
         let mut incoming = endpoint.listen();
         let mut messages = incoming
             .next()
             .await
-            .ok_or_else(|| anyhow!("Missing expected incomming connection"))?;
+            .ok_or_else(|| anyhow!("Missing expected incoming connection"))?;
         let connecting_peer = messages.remote_addr();
         println!("Incoming connection from: {}", &connecting_peer);
 
-        let message = messages
+        let _message = messages
             .next()
             .await
             .ok_or_else(|| anyhow!("Missing expected incomming message"))?;
-        println!("Responded to peer with EchoService response");
-
-        println!("Waiting for messages...");
-        let (mut bytes, mut send, mut recv) = if let Message::BiStream {
-            bytes, send, recv, ..
-        } = message
-        {
-            (bytes, send, recv)
-        } else {
-            println!("Only bidirectional streams are supported in this example");
-            bail!("Only bidirectional streams are supported in this example");
-        };
-
-        loop {
-            println!(
-                "Got message: {}",
-                std::str::from_utf8(&bytes[..])
-                    .map_err(|err| anyhow!("Bytes received cannot read as UTF8 string: {}", err))?
-            );
-
-            println!("Enter message:");
-            let input = read_from_stdin().await;
-            send.send_user_msg(Bytes::from(input)).await?;
-            bytes = recv.next().await?;
-        }
-    } else {
-        println!("Echo service complete");
-        let node_addr = bootstrap_nodes[0];
-        let (connection, _) = endpoint.connect_to(&node_addr).await?;
-        let (mut send, mut recv) = connection.open_bi().await?;
-
-        loop {
-            println!("Enter message:");
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input)?;
-            send.send_user_msg(Bytes::from(input)).await?;
-
-            let bytes = recv.next().await?;
-            println!(
-                "Got message: {}",
-                std::str::from_utf8(&bytes[..])
-                    .map_err(|err| anyhow!("Bytes received cannot read as UTF8 string: {}", err))?
-            );
-        }
+        println!("Responded to peer with EchoService response\n");
     }
-}
-
-async fn read_from_stdin() -> String {
-    let mut input = String::new();
-    let stdin = tokio::io::stdin();
-    let mut buf_reader = tokio::io::BufReader::new(stdin);
-    buf_reader.read_line(&mut input).await.unwrap_or(0);
-    input
 }
