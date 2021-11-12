5bce037e5ccd446f3cb7ba216c2a526fc8a4d146
Chris Connelly <chris.connelly@maidsafe.net>
2021-11-12 16:02:39 +0000

    feat: add a convenient `retry` method to `BackoffRef`
    This makes it a bit easier to maintain a 'retry context' over a number
    of operations.

--- src/backoff.rs
+++ src/backoff.rs
@@ -1,7 +1,7 @@
 //! Utilities for working with retries driven by the [`backoff`](::backoff) crate.
 
 use backoff::backoff::Backoff;
-use std::time::Duration;
+use std::{future::Future, time::Duration};
 
 /// A wrapper for an `&mut impl `[`Backoff`].
 ///
@@ -15,6 +15,34 @@ impl<'b, B: Backoff> BackoffRef<'b, B> {
     /// Construct a `BackoffRef` from a mutable reference to an implementor of [`Backoff`].
     pub fn new(backoff: &'b mut B) -> Self {
         Self(backoff)
+    }
+
+    /// Retry `operation` until permanent error or exhausting `self`.
+    pub async fn retry<Op, Fut, Ok, Err>(&mut self, mut operation: Op) -> Result<Ok, Err>
+    where
+        Op: FnMut(BackoffRef<'_, B>) -> Fut,
+        Fut: Future<Output = Result<Ok, backoff::Error<Err>>>,
+    {
+        loop {
+            // try the operation
+            match operation(BackoffRef(&mut self.0)).await {
+                Ok(value) => return Ok(value),
+                Err(backoff::Error::Transient(error)) => {
+                    // retry the transient error, if we have time remaining
+                    if let Some(duration) = self.next_backoff() {
+                        tokio::time::sleep(duration).await;
+                        continue;
+                    } else {
+                        // we're out of time, bail
+                        return Err(error);
+                    }
+                }
+                Err(backoff::Error::Permanent(error)) => {
+                    // we cannot retry permanent errors here
+                    return Err(error);
+                }
+            }
+        }
     }
 }
 
